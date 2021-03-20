722577951fc547e7d3368913125d76eaea434dcc
RivaD2 <rivkadavidowski@gmail.com>
2021-03-20 15:17:17 -0700

    looped through data and appended images to DOM

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const getGifs = async () => {
   try{
     const result = await axios.get('http://localhost:3000/gifs');
     console.log('what is result', result);
-
+    const container = document.getElementById("gif-container");
+    const children = result.data.data.forEach(gif => {
+      const child = document.createElement("img");
+      child.src = gif.images.downsized.url;
+      console.log('checking src', child.src);
+      container.append(child);
+    });
   } catch (err) {
     console.log(err);
   }
