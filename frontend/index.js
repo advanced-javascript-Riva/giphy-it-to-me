0e241d9f5e95ec8eff9b5e8f48ce91e06b36a6ab
RivaD2 <rivkadavidowski@gmail.com>
2021-10-15 13:24:36 -0700

    tbody addition

--- frontend/index.js
+++ frontend/index.js
@@ -48,7 +48,7 @@ const getGifsByTag = async terms => {
   terms = ['pizza', 'robots', 'coffee', 'magic', 'gourmet', 'wizards'];
   try {
     const result =  await axios.get(`http://localhost:3000/tags/related/${terms}`);
-    showGifsByTag(result);
+    showTagTable(result);
   } catch (err) {
     console.error(err);
   }
@@ -57,66 +57,42 @@ const getGifsByTag = async terms => {
 // Initial gifs on load
 getGifs('plants');
 
-// Todo: create accordian and table for funzies/practice for interview
-// Todo: Break apart this big function so that table logic is separate from showing just tag results
-showGifsByTag = result => {
-  const tagContainer = document.getElementById('tag-container');
-
-  const tagHeader = document.createElement('h3');
-  tagHeader.className="tag-results";
-  let tagHeaderTitle = 'Tag Results';
-  tagHeader.append(tagHeaderTitle);
-  tagContainer.append(tagHeader);
-
+// Tag table
+showTagTable = result => {
   const allTagNames = result.data.data.map(tagnames => tagnames.name);
-  const allChildrenOfTagContainer = allTagNames.forEach(tags => {
-    const namesOfTags = document.createElement('p');
-    namesOfTags.className = "tag-names";
-    namesOfTags.append(tags);
-    tagContainer.append(namesOfTags);
-  });
 
-// Table creation for all tag names
-// I could set table to be an empty string and then create rows variable and set it to a value
-// I could also use a for loop to loop through rows and another loop for columns;
-const tableOfTags = document.createElement('table');
-tableOfTags.style.border = '2px solid blue';
-tableOfTags.className = 'tag-table';
+  // I could set table to be an empty string and then create rows variable and set it to a value
+  // I could also use a for loop to loop through rows and another loop for columns;
+  const tableOfTags = document.createElement('table');
+  tableOfTags.setAttribute('class', 'tags-results-table');
+  tableOfTags.style.border = '2px solid blue';
 
+  const tableTagBody = document.createElement('tbody');
+  tableOfTags.appendChild(tableTagBody);
 
-/*
- save for now:
-function makeTable(rowCount, columnCount) {
-  for(let i = 0; i <= rowCount; i++) {
-    let allRows = document.createElement('tr');
-    allRows.style.border = '3px solid blue';
-    console.log('allRows', allRows);
-    for(let columns = 0; columns < columnCount; columns++) {
-      let tableData = document.createElement('td');
-      tableData.style.border = '3px solid blue';
-      tableData.style.color = 'black';
-      console.log(tableData);
-    }
-  }
-}
-console.log(makeTable(2, 3));
-*/
+  const tagTableHeader = document.createElement('th');
+  tagTableHeader.textContent = 'Tag Results';
+  tableTagBody.appendChild(tagTableHeader);
 
-// I have tr for each tag, which is fine, try to get 2 tr and 2td
-    allTagNames.forEach(tag => {
-     let tableRows = document.createElement('tr');
-     tableRows.style.border = '3px solid blue';
+  allTagNames.forEach(tag => {
+    let tableRows = document.createElement('tr');
+    tableRows.style.border = '3px solid blue';
+    tableRows.style.padding = '6px';
 
-     let tableData = document.createElement('td');
-     tableData.style.border = '3px solid blue';
-     tableData.style.color = 'black';
-     tableData.textContent = tag;
+    let tableData = document.createElement('td');
+    tableData.setAttribute('class', 'tag-data');
+    tableData.style.border = '3px solid blue';
+    tableData.style.color = 'black';
+    tableData.textContent = tag;
 
-     tableOfTags.appendChild(tableRows);
-     tableRows.appendChild(tableData);
-     document.body.append(tableOfTags);
+    tableTagBody.appendChild(tableRows);
+    tableRows.appendChild(tableData);
+    document.body.append(tableOfTags);
   });
 }
+
+// Accordian
+
 
 // Triggering an event on Enter key pressed to show search results
 const enterKeyPressed = document.getElementById('search-bar')
